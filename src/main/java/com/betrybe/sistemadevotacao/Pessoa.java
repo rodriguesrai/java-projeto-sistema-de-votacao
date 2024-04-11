9f2aafe47a544ea78769feb7ddcd6ba3a951c16b
rodriguesrai <rairodrigues_sf@hotmail.com>
2024-04-11 15:53:47 -0300

    req 3

--- src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
+++ src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
@@ -5,25 +5,12 @@ package com.betrybe.sistemadevotacao;
  */
 public abstract class Pessoa {
 
-  /**
-   * The Nome.
-   */
   protected String nome;
 
-  /**
-   * Gets nome.
-   *
-   * @return the nome
-   */
   public String getNome() {
     return nome;
   }
 
-  /**
-   * Sets nome.
-   *
-   * @param nome the nome
-   */
   public void setNome(String nome) {
     this.nome = nome;
   }
