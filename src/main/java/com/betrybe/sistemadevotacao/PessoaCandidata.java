8c72a86420b9797a4880880ff744818eca1b9a2f
rodriguesrai <rairodrigues_sf@hotmail.com>
2024-04-11 15:39:30 -0300

    req2

--- src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -3,6 +3,60 @@ package com.betrybe.sistemadevotacao;
 /**
  * The type Pessoa candidata.
  */
-public class PessoaCandidata {
+public class PessoaCandidata extends Pessoa {
 
+  /**
+   * The Numero.
+   */
+  int numero;
+  /**
+   * The Votos.
+   */
+  int votos;
+
+  /**
+   * Instantiates a new Pessoa candidata.
+   *
+   * @param nome   the nome
+   * @param numero the numero
+   */
+  public PessoaCandidata(String nome, int numero) {
+    this.nome = nome;
+    this.numero = numero;
+    this.votos = 0;
+  }
+
+  /**
+   * Gets numero.
+   *
+   * @return the numero
+   */
+  public int getNumero() {
+    return numero;
+  }
+
+  /**
+   * Gets votos.
+   *
+   * @return the votos
+   */
+  public int getVotos() {
+    return votos;
+  }
+
+  /**
+   * Sets numero.
+   *
+   * @param numero the numero
+   */
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+  /**
+   * Receber voto.
+   */
+  public void receberVoto() {
+    this.votos++;
+  }
 }
