c1b8b264f359b494d12e3013a0d9431034bb144e
rodriguesrai <rairodrigues_sf@hotmail.com>
2024-04-11 16:19:18 -0300

    req5

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -1,6 +1,5 @@
 package com.betrybe.sistemadevotacao;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 /**
@@ -14,12 +13,27 @@ public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
 
   @Override
   public void cadastrarPessoaCandidata(String nome, int numero) {
+    if (pessoasCandidatas
+        .stream()
+        .anyMatch(pessoaCandidata -> pessoaCandidata.getNumero() == numero)) {
+      System.out.println("Número da pessoa candidata já utilizado!");
+    }
+
+    PessoaCandidata pessoaCandidata = new PessoaCandidata(nome, numero);
+    pessoasCandidatas.add(pessoaCandidata);
 
   }
 
   @Override
   public void cadastrarPessoaEleitora(String nome, String cpf) {
+    if (pessoasEleitoras
+        .stream()
+        .anyMatch(pessoaEleitora -> pessoaEleitora.getCpf().equals(cpf))) {
+      System.out.println("Pessoa eleitora já cadastrada!");
+    }
 
+    PessoaEleitora pessoaEleitora = new PessoaEleitora(nome, cpf);
+    pessoasEleitoras.add(pessoaEleitora);
   }
 
   @Override
